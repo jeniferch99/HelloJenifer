456e07cb3a15dfb51ad177be626a784a819bf509
Jenifer <jeniferch99@gmail.com>
2018-09-14 22:44:21 -0700

    intialize commit

---
+++ HelloJenifer/app/src/main/java/com/example/jenifer/hellojenifer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.jenifer.hellojenifer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+}
